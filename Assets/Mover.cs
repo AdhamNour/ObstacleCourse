956eaad72d6ce40d35e663c7f2fb8427de969f11
Adham Nour <adhamnourelwaffaa@gmail.com>
2022-06-24 19:35:22 +0200

    finished lecture 13

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    private double accumlatedTime=0;
+    private double accumlatedTime = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +16,8 @@ public class Mover : MonoBehaviour
     void Update()
     {
         accumlatedTime += Time.deltaTime;
-        transform.position=new Vector3((float)Math.Cos(accumlatedTime),0,(float)Math.Sin(accumlatedTime));
-
+        Vector3 HorizontalRotation = new Vector3((float)Math.Cos(accumlatedTime), 0, (float)Math.Sin(accumlatedTime));
+        Vector3 VerticalRotation = new Vector3(0, 1+(float)Math.Cos(accumlatedTime), (float)Math.Sin(accumlatedTime));
+        transform.position = HorizontalRotation + VerticalRotation;
     }
 }
