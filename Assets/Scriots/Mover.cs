0eadc48b1be93506fbad38169deb4e978d596114
Adham Nour <adhamnourelwaffaa@gmail.com>
2022-06-24 20:27:27 +0200

    finished lecture 15

--- Assets/Scriots/Mover.cs
+++ Assets/Scriots/Mover.cs
@@ -5,14 +5,7 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] private double accumlatedTime = 0;
-
-    [SerializeField] private float HorizontalRadius = 1;
-
-    [SerializeField] private float VerticalRadius = 1;
-    [SerializeField] private double HorizontalSpeed = 1;
-    [SerializeField] private double VirticalSpeed = 1;
-
+    [SerializeField] private float Speed = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,13 +14,8 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        accumlatedTime += Time.deltaTime;
-        Vector3 HorizontalRotation = new Vector3(
-            HorizontalRadius * (float)Math.Cos(accumlatedTime * HorizontalSpeed * Math.PI), 0,
-            HorizontalRadius * (float)Math.Sin(accumlatedTime * HorizontalSpeed * Math.PI));
-        Vector3 VerticalRotation = new Vector3(0,
-            1 + VerticalRadius * (float)Math.Cos(accumlatedTime * VirticalSpeed * Math.PI),
-            VerticalRadius * (float)Math.Sin(accumlatedTime * VirticalSpeed * Math.PI));
-        transform.position = HorizontalRotation + VerticalRotation;
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime*Speed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime*Speed;
+        transform.Translate(xValue, 0, zValue);
     }
 }
